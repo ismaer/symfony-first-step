56c8eb33b7be623c2c454c02f48bfb2b0a0a2439
Ismaer <ismaelgomez.98@gmail.com>
2018-09-05 10:28:39 -0500

    Inicio de proyecto curso symfony  con git

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route()
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! Wo');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug){
+        return new Response(sprintf('Future page to show one space article: %s', $slug));
+    }
+}
